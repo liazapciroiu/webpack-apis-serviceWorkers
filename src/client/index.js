ed5ba8f045388e79e4d8260ed304a3f9e0d192dc
Lia Zapciroiu <lzapciroiu@deloittece.com>
2020-05-11 16:35:33 +0300

    feat: setup webpack

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import { checkForName } from './js/nameChecker';
+import { handleSubmit } from './js/formHandler';
+import './styles/main.scss';
+
+console.log(checkForName);
+
+alert("I EXIST")
+console.log("CHANGE!!");
